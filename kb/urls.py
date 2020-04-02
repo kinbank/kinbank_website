37a6c63dd7d26cf004e141cb3fa8c6576f67b65f
Sam Passmore <passmore.sam@gmail.com>
2020-04-02 18:03:10 +0100

    new pages + views

--- kb/urls.py
+++ kb/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	#url(r'^$', views.home, name='home'),
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('languages/', views.languages, name='languages'),
+    path('phylogeny/', views.phylogeny, name='phylogeny'),
 ]
--- kb/views.py
+++ kb/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .models import Forms
 
 # Create your views here.
 def home(request):
-    return render(request, 'kb/home.html', {})
+	n_languages = Forms.objects.values('language_id').distinct().count()
+	return render(request, 'kb/home.html', {'test': n_languages})
+
+def languages(request):
+	n_languages = Forms.objects.values('language_id').distinct().count()
+	return render(request, 'kb/languages.html', {'test': n_languages})
+
+def phylogeny(request):
+	n_languages = Forms.objects.values('language_id').distinct().count()
+	return render(request, 'kb/phylogeny.html', {'test': n_languages})
+
+def about(request):
+	n_languages = Forms.objects.values('language_id').distinct().count()
+	return render(request, 'kb/about.html', {'test': n_languages})
