8525542aba39c0aa5e00b5d25e5029730c717cda
Sam Passmore <passmore.sam@gmail.com>
2020-04-05 16:41:41 +0100

    skeleton site

--- kb/urls.py
+++ kb/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('languages/', views.languages, name='languages'),
+    path('post/<str:pk>/', views.language_detail, name='language_detail'),
     path('phylogeny/', views.phylogeny, name='phylogeny'),
 ]
--- kb/views.py
+++ kb/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Forms
+from django.shortcuts import render, get_object_or_404
+from .models import Forms, Languages
 
 # Create your views here.
 def home(request):
@@ -7,8 +7,15 @@ def home(request):
 	return render(request, 'kb/home.html', {'test': n_languages})
 
 def languages(request):
-	n_languages = Forms.objects.values('language_id').distinct().count()
-	return render(request, 'kb/languages.html', {'test': n_languages})
+	languages = Languages.objects.only('glottolog_name', 'glottocode').distinct()
+	#languages = [{l['glottocode']: l['glottolog_name']} for l in languages]
+	return render(request, 'kb/languages.html', {'languages': languages})
+
+def language_detail(request, pk):
+	post = get_object_or_404(
+		Forms.objects.filter('language_id' == pk).distinct().count()
+		, pk=pk)
+	return render(request, 'kb/language_detail.html', {'post': post})
 
 def phylogeny(request):
 	n_languages = Forms.objects.values('language_id').distinct().count()
